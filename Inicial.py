20ff2509cd2b7e38731b8f808e6119ae1299d602
Rodrigo Henriquez <58607986+rodrigorhp@users.noreply.github.com>
2020-10-26 15:52:09 -0600

    Create Inicial.py

---
+++ Inicial.py
@@ -0,0 +1,22 @@
+#matricula y nombre 1
+#matricula y nombre 2
+#proposito del programa
+
+#funcion1  calcular área del rectángulo
+
+
+#funcion2  calcular perímetro del rectángulo
+
+
+#instrucciones de accion
+#pedir datos
+print("medida de lado 1 del rectángulo")
+l1 = float(input())
+
+print("medida de lado 2 del rectángulo")
+l2 = float(input())
+
+#desplegar calculo funcion1
+
+#desplegar calculo funcion 2
+
