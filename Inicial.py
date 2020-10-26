7eaba043121f18d4375c5bb328558309989040e9
Rodrigo <A00827198@itesm.mx>
2020-10-26 16:28:30 -0600

    Finalizado

--- Inicial.py
+++ Inicial.py
@@ -5,19 +5,23 @@
 
 #funcion1  calcular área del rectángulo
 def calcularArea(l1, l2):
-    return l1 * l2
+    return (l1 * l2)
 
 #funcion2  calcular perímetro del rectángulo
+def calcularPerimetro(l1,l2):
+    return(2*l1+2*l2)
 
 #instrucciones de accion
 #pedir datos
 print("medida de lado 1 del rectángulo")
 l1 = float(input())
 
-print("medida de lado 2 del rectángulo")
+print("medida de lado 2 del rectángulo")2
 l2 = float(input())
 
 #desplegar calculo funcion1
-
+area = calcularArea(l1,l2)
+print(area)
 #desplegar calculo funcion 2
-
+perimetro = calcularPerimetro(l1,l2)
+print(perimetro)
